1ddd2a255c4ac9a9c4bf8a4198a8811550ed6372
Richard Neves <ricknevesbc@icloud.com>
2024-07-12 09:44:35 -0300

    Initial Project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  plugins: ['eslint-plugin-import-helpers'],
+  extends: ['@rocketseat/eslint-config/react', 'plugin:prettier/recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'off',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: false,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="max-w-3xl px-6">
+        <p className="text-lg text-zinc-300">
+          Convide seus amigos e planeje sua próxima viagem!
+        </p>
+      </div>
+    </div>
+  )
+}
